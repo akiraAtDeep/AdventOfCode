6a4e4e8521a6de6393ad232dc16290beaf48d02c
akiraAtDeep <eugenio.dorigati@gmail.com>
2021-01-13 08:14:35 +0100

    risolta la prima parte del giorno 24, la linea vuota alla fine del file veniva vista come una rotazione della piastrella 0-0.

--- 2020/day24/1.js
+++ 2020/day24/1.js
@@ -3,63 +3,43 @@ const { toNamespacedPath, parse } = require('path');
 
 fs = require('fs')
 
-fs.readFile('input_test.txt', 'utf8', function (err,data) {
+fs.readFile('input.txt', 'utf8', function (err,input) {
   if (err) {
     return console.log(err);
   }
-  day(data);
-});
-
-function day(input) {
-  input = input.split('\n');
-
+  input = input.trim().split('\n');
   var coords = new Object();
 
   input.forEach(element => {
-    element = element.replace(/nw/g,'q');
-    element = element.replace(/sw/g,'z');
-    element = element.replace(/ne/g,'r');
-    element = element.replace(/se/g,'x');
-    element = element.split('');
-    var x = 0, y = 0;
-    element.forEach(element => {
-      switch(element) {
-        case ('q'): //nw
-          x -= 0.5;
-          y--;
+    var q = 0, r = 0;
+    for (var i = 0; i < element.length; i++){
+      switch(element[i]) {
+        case('n'):
+          if (element[i+1] === 'w') r--;
+          else if (element[i+1] === 'e') r--, q++;
+          i++;
+          break;
+        case('s'):
+          if (element[i+1] === 'w') q--, r++;
+          else if (element[i+1] === 'e') r++;
+          i++;
           break;
         case ('w'):
-          x--;
-          break;
-        case ('z'): //sw
-          x -= 0.5;
-          y++;
-          break;
-        case ('x'): //se
-          x += 0.5;
-          y++;
+          q--;
           break;
         case ('e'):
-          x++;
-          break;
-        case ('r'): //ne
-          x += 0.5;
-          y--;
+          q++;
           break;
       };
-    });
-    if (coords[x+':'+y] === undefined) coords[x+':'+y] = 0;
-    coords[x+':'+y]++;
+    };
+    if (coords[q+'.'+r] === undefined) coords[q+'.'+r] = 0;
+    coords[q+'.'+r]++;
   });
 
   var count = 0;
-
-  //console.log(coords);
-  
   for (var key in coords) {
     if (coords[key] % 2 !== 0) count++;
   }
 
   console.log(count);
-
-}
+});
