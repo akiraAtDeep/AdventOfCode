c5cfae206c2dfdd5da3b1a57738b94cab5843050
akiraAtDeep <eugenio.dorigati@gmail.com>
2022-12-06 06:49:51 +0100

    risoloto 2022 day 06

--- 2022/day06/solution.js
+++ 2022/day06/solution.js
@@ -8,6 +8,39 @@ fs.readFile('./input.txt', 'utf8', function (err,input) {
     return console.log(err);
   }
   //input = input.trim().split('\n');
+  var start ,startmsg ;
+
+  for (var i=0; i<input.length-3; i++) {
+    if (start === undefined) {
+      var sfound = testString(i,4);
+      if (sfound) {
+        start = i+4;
+      }
+    }
+    if (startmsg === undefined) {
+      var mfound = testString(i,14);
+      if (mfound) {
+        startmsg = i+14;
+      }
+    }
+  }
+
+  console.log(start);
+  console.log(startmsg);
+
+  function testString(index,n) {
+    var test = input.slice(index,index+n);
+    var found = true;
+    for (var a=0; a<test.length-1; a++) {
+      for (var b=a+1; b<test.length; b++) {
+        if (test[a]===test[b]) {
+          found = false;
+          break;
+        }
+      }
+    }
+    return found;
+  }
   
 
 });
